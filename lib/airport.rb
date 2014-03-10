a5c0d1014af40d12d839df5a491c75441c7cd22e
itsmurasaki <itsmurasaki@gmail.com>
2014-03-10 00:46:15 +0000

    added airport capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,9 @@
 class Airport
-  def initialize
+  
+  DEFAULT_CAPACITY = 100
+
+  def initialize(capacity = {})
+    @capacity = capacity.fetch(:capacity, DEFAULT_CAPACITY)
     @planes = []
   end
 
@@ -8,7 +12,16 @@ class Airport
   end
 
   def add(plane)
+    raise "Airport is full! Can't accept other planes." if full?
     @planes << plane
+  end
+
+  def takeoff(plane)
+    @planes.delete(plane)
+  end
+
+  def full?
+    plane_count == @capacity
   end
 
 end
