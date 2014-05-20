47ca0dd31c5564df2e4cdcac327abe674e3f9fda
itsmurasaki <itsmurasaki@gmail.com>
2014-05-20 23:59:59 +0100

    planes should be able to take off

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -13,5 +13,11 @@ describe Plane do
     plane.land
     expect(plane).not_to be_flying
   end
+
+  it "should be able to take off when in the airport" do
+    plane.land
+    plane.take_off
+    expect(plane).to be_flying
+  end
 	
 end
